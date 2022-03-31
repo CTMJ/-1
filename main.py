e5559475c915a30bcd3ca2cd34f9e43b4bc7a913
CTMJ <69998314+CTMJ@users.noreply.github.com>
2022-03-31 23:29:39 +1100

    FractalGeometry-soil01

---
+++ main.py
@@ -0,0 +1,76 @@
+#PSP boxCounting
+from __future__ import print_function, division
+import math
+from PIL import Image
+import matplotlib.pyplot as plt
+
+def identifyPores(colorGrid, width, height, myColor):
+    print("identifying pores ...")
+    nrPores = 0
+    rgbThreshold = 128
+    for x in range(width):
+        for y in range(height):
+            color = colorGrid[x, y]
+            if ((color[0] > rgbThreshold) and (color[1] > rgbThreshold) and (color[2] > rgbThreshold)):
+                nrPores += 1
+                colorGrid[x, y] = myColor
+    return (nrPores, colorGrid)
+
+def boxCounting(colorGrid, width, height, size, threshold, myColor):
+    nrPixelsThreshold = threshold * size * size
+    nrXBoxes = int(width / size)
+    nrYBoxes = int(height / size)
+    nrOccupiedBoxes = 0
+    for i in range(nrXBoxes):
+        for j in range(nrYBoxes):
+            nrPoresPixels = 0
+            for dx in range(size):
+                for dy in range(size):
+                    x = i * size + dx
+                    y = j * size + dy
+                    color = colorGrid[x, y]
+                    if color == myColor: nrPoresPixels += 1
+            if (nrPoresPixels > nrPixelsThreshold):
+                nrOccupiedBoxes += 1
+
+    #normalized one-dimensional size
+    L = 1./math.sqrt(nrXBoxes*nrYBoxes)
+    #fractal dimension
+    if (nrOccupiedBoxes > 0):
+        D = math.log(nrOccupiedBoxes) / math.log(L)
+    else:
+        D = 0
+    print("box size =", size, "pores =", nrOccupiedBoxes, " L =", format(L, '.3f'), " D =", format(D, '.3f'))
+    return(nrOccupiedBoxes)
+
+def main():
+    picture = Image.open("soil_image.jpg")
+    colorGrid = picture.load()
+    [width, height] = picture.size
+    nrPixels = width * height
+    print("width =", width)
+    print("height =", height)
+
+    myColor = (255, 255, 255)    #red, green, blue 0-255
+    nrPores, ColorGrid = identifyPores(colorGrid, width, height, myColor)
+    poresPercentage = float(nrPores) / float(nrPixels) * 100.
+    print("% of pores =", format(poresPercentage, '.3f'))
+    picture.show()
+    plt.xlabel('Box size', fontsize=16, labelpad=5)
+    plt.ylabel('N', fontsize=16, labelpad=5)
+    plt.ion()
+    size = 200    #pixels
+    threshold = 0.2
+    while (size >= 1):
+        nrOccupiedBoxes = boxCounting(colorGrid, width, height, size, threshold, myColor)
+        if (nrOccupiedBoxes > 0):
+            plt.loglog(size, nrOccupiedBoxes, 'ko')
+            plt.draw()
+        size = int(size / 2)
+
+    plt.ioff()
+    plt.show()
+
+main()
+
+
